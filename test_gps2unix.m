8d9cedae51d2deb70da95a4e91bd81626afc49d5
jhuai <huai.3@osu.edu>
2023-11-07 21:54:35 +0800

    fix unix2gps

--- test_gps2unix.m
+++ test_gps2unix.m
@@ -4,10 +4,28 @@ function test_gps2unix()
     gpstime = gpsweek * 7 * 24 * 3600 + gpssec;
     fprintf('gps time %.9f\n', gpstime);
     unix = gps2unix(gpstime);
+    gps2 = unix2gps(unix);
+    assert(gpstime - gps2 == 0);
     unix2 = gpstime + 315964800 - 18;
     unix3 = 1698226837.24; % per https://www.andrews.edu/~tzs/timeconv/timeconvert.php
-    fprintf('unix %.9f- unix2 %.9f = %.9f, unix - unix3 = %.9f\n', ...
-        unix, unix2, unix - unix2, unix- unix3);
+    fprintf('unix %.9f, unix - unix2 = %.9f, unix - unix3 = %.9f\n', ...
+        unix, unix - unix2, unix- unix3);
     assert(unix - unix2 == 0);
     assert(unix - unix3 == 0);
+
+    unix = 1662203356.64;
+    gps = unix2gps(unix);
+    unix2 = gps + 315964800 - 18;
+    unix3 = gps2unix(gps);
+    assert(unix - unix2 == 0);
+    assert(unix2 - unix3 == 0);
+
+    gpsweek = 2287;
+    gpssec = 31929.7;
+    gpstime = gpsweek * 7 * 24 * 3600 + gpssec;
+    unix = gps2unix(gpstime);
+    gps2 = unix2gps(unix);
+    assert(gpstime - gps2 == 0);
+    unix2 = gpstime + 315964800 - 18;
+    assert(unix - unix2 == 0);
 end
--- unix2gps.m
+++ unix2gps.m
@@ -3,12 +3,13 @@
 function gpsTime = unix2gps(unixTime)
 % code adapted from https://www.andrews.edu/~tzs/timeconv/timealgorithm.html
     % Add offset in seconds
-    if (fmod(unixTime, 1) != 0)
-        unixTime = unixTime - 0.5;
-        isLeap = 1;
-    else
-        isLeap = 0;
-    end
+    % if (rem(unixTime, 1) ~= 0)
+    %     unixTime = unixTime - 0.5;
+    %     isLeap = 1;
+    % else
+    %     isLeap = 0;
+    % end
+    isLeap = 0;
     gpsTime = unixTime - 315964800;
     nleaps = countleaps(gpsTime, 'unix2gps');
     gpsTime = gpsTime + nleaps + isLeap;
